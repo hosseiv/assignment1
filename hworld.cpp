b0a3299721b09c1c55fec638e0d5618b4ec5a564
hosseiv <zohrab.h00f@gmail.com>
2025-03-07 15:43:39 +0330

    adding info to hworld

--- hworld.cpp
+++ hworld.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+    cout<<"Hossein Hosseini Fard"<<endl;
+    cout<<"403101723"<<endl;
     cout<<"Hello World!"<<endl;
     return 0;
 }
